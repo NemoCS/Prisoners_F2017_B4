c1fede08571bb3db86f9795b10b0a1a5cc58b449
Justin-Merkel <jumerkel@student.linnmar.org>
2016-09-28 14:40:59 -0500

    added names
    thats it

--- team6.py
+++ team6.py
@@ -5,6 +5,7 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
+#Justin Merkel, Jack
 
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
