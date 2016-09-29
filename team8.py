7768282052bd751eda30dad1276172cef9798775
Hahuntington <hahuntington@linnmar.local>
2016-09-29 15:28:20 -0500

    Rough Draft of Strategy
    changed team name and descrption

--- team8.py
+++ team8.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 #Zoey and Haley's stuff
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Muffin Robots' # Only 10 chars displayed.
+strategy_name = 'Life does not need a strategy'
+strategy_description = 'If they CC in last 2 rounds, betray. If they bb in last 2 rounds, collude. If other, betray.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +17,6 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,14 +24,20 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if '' in my_history:
+        return 'c'
+    if 'cc' in their_history[-2:]:
+        return 'b'
+    elif 'bb' in their_history[-2:]:
+        return 'c'
+    else:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
+    Returns True or False, depending on whether result was as expected.
     '''
     real_result = move(my_history, their_history, my_score, their_score)
     if real_result == result:
