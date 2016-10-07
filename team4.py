1f7e7d6d635b6bcf050de1a4194c2e0cdf600721
Gavin Bading <gavintbading37@gmail.com>
2016-10-07 14:08:00 -0500

    MOAR team 4 things
    slightly edited strategy name, and cleaned up some more code

--- team4.py
+++ team4.py
@@ -1,15 +1,8 @@
 team_name = 'Trumpence' # Only 10 chars displayed.
-strategy_name = 'Making America Great Again'
+strategy_name = 'making America great again'
 strategy_description = 'Be cooperative in the beginning; if betrayed two times, betray back; if betrayed again, keep betraying'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
